feb1ded52bbc6a98610f62084fb7276ddbe518d3
PavelK6896 <79208776896@yandex.ru>
2021-02-25 21:30:59 +0300

    add singleton

---
+++ Singleton/src/main/java/app/web/pavelk/pattern1/DemoMultiThread2.java
@@ -0,0 +1,30 @@
+package app.web.pavelk.pattern1;
+
+
+public class DemoMultiThread2 {
+    public static void main(String[] args) {
+        System.out.println("If you see the same value, then singleton was reused (yay!)" + "\n" +
+                "If you see different values, then 2 singletons were created (booo!!)" + "\n\n" +
+                "RESULT:" + "\n");
+        Thread threadFoo = new Thread(new ThreadFoo());
+        Thread threadBar = new Thread(new ThreadBar());
+        threadFoo.start();
+        threadBar.start();
+    }
+
+    static class ThreadFoo implements Runnable {
+        @Override
+        public void run() {
+            Singleton2 singleton2 = Singleton2.getInstance("FOO");
+            System.out.println(singleton2.value);
+        }
+    }
+
+    static class ThreadBar implements Runnable {
+        @Override
+        public void run() {
+            Singleton2 singleton2 = Singleton2.getInstance("BAR");
+            System.out.println(singleton2.value);
+        }
+    }
+}
