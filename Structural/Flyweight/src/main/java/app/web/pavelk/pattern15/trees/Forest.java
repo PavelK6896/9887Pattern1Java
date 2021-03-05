6aed084f8465da1845dcf1215f13865aaa27dd11
PavelK6896 <79208776896@yandex.ru>
2021-03-06 00:15:34 +0300

    add flyweight

---
+++ Structural/Flyweight/src/main/java/app/web/pavelk/pattern15/trees/Forest.java
@@ -0,0 +1,28 @@
+package app.web.pavelk.pattern15.trees;
+
+
+import app.web.pavelk.pattern15.forest.Tree;
+import app.web.pavelk.pattern15.forest.TreeFactory;
+import app.web.pavelk.pattern15.forest.TreeType;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Forest extends JFrame {
+  private List<Tree> trees = new ArrayList<>();
+
+  public void plantTree(int x, int y, String name, Color color, String otherTreeData) {
+    TreeType type = TreeFactory.getTreeType(name, color, otherTreeData);
+    Tree tree = new Tree(x, y, type);
+    trees.add(tree);
+  }
+
+  @Override
+  public void paint(Graphics graphics) {
+    for (Tree tree : trees) {
+      tree.draw(graphics);
+    }
+  }
+}
